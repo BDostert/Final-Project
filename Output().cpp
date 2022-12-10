e0b914919498aec3ebbdc86f22670a048a2f4a71
BDostert <116378525+BDostert@users.noreply.github.com>
2022-12-09 19:33:49 -0500

    Update Output().cpp
    Output function without dummy main

--- Output().cpp
+++ Output().cpp
@@ -20,31 +20,18 @@ void Output(int userDate[DATE_ARRAY_SIZE], resultsType results) {
 	ofstream fsOut;
 	fsOut.open("results.txt");
 	if (fsOut.is_open()) {
+		// outputs results to screen 
 		cout << "On " << userDate[1] << "/" << userDate[2] << "/" << userDate[0] << " "
 			<< results.name << " was president and served " << results.terms
 			<< " from " << results.startDate[1] << "/" << results.startDate[2] << "/" << results.startDate[0]
-			<< " to " << results.endDate[1] << "/" << results.endDate[2] << "/" << results.endDate[0] << ".\n";
-
+			<< " to " << results.endDate[1] << "/" << results.endDate[2] << "/" << results.endDate[0] << "."<< endl;
+		// outputs results to results.txt file
 		fsOut << "On " << userDate[1] << "/" << userDate[2] << "/" << userDate[0] << " "
 			<< results.name << " was president and served " << results.terms
 			<< " from " << results.startDate[1] << "/" << results.startDate[2] << "/" << results.startDate[0]
-			<< " to " << results.endDate[1] << "/" << results.endDate[2] << "/" << results.endDate[0] << ".\n";
+			<< " to " << results.endDate[1] << "/" << results.endDate[2] << "/" << results.endDate[0] << "."<< endl;
 	}
 }
-
-
-
-int main() {    
-
-	resultsType results{   // filled struct for testing Output
-		{1789,4,30}, {1797,3,3}, "George Washington", "2 full terms"
-	};
-
-	int inputDate[DATE_ARRAY_SIZE] = { 1792, 1, 1 };
-
-	Output(inputDate, results);
-}
-
 
 
 
